af50949915ffeaf3597fcbedfff2580bafc45abe
Richard Naing <richardbbnaing@gmail.com>
2026-01-20 09:53:32 -0700

    Arithmetic_Assignments.js

---
+++ PlayGround/Arithmetic_Assignments.js
@@ -0,0 +1,11 @@
+//Arithmetic + Assignment 1
+let score = 40;
+score += 10;
+score *= 2;
+score -= 15;
+console.log(score); // Find the expected output
+
+//Comparison (== vs ===)
+console.log(5 == "5"); // why ture? (because only look up number and equal number or not)
+console.log(5 === "5"); // Why false? (because not only look up number, also look up data type)
+console.log(0 === false); // Why true? (because not equal number)
