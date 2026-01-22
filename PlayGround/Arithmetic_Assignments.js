071bb5c5767f8a6cc0a53f1ae919db29d76586bf
Richard Naing <richardbbnaing@gmail.com>
2026-01-21 19:45:50 -0700

    Update the Operators_PG

--- PlayGround/Arithmetic_Assignments.js
+++ PlayGround/Arithmetic_Assignments.js
@@ -9,3 +9,11 @@ console.log(score); // Find the expected output
 console.log(5 == "5"); // why ture? (because only look up number and equal number or not)
 console.log(5 === "5"); // Why false? (because not only look up number, also look up data type)
 console.log(0 === false); // Why true? (because not equal number)
+
+//Increment/Decrement
+let likes = 15;
+likes++;
+console.log(likes);
+++likes;
+console.log(likes);
+console.log(likes--);
---
+++ PlayGround/Operators_PG.js
@@ -0,0 +1,4 @@
+let number = 150000;
+let number2 = 300000;
+
+console.log("add number =", number + number2);
