98246a685804f80dbf56e4f0ff458a524d11bf4b
Richard Naing <richardbbnaing@gmail.com>
2026-01-25 15:08:45 -0700

    Strings update

---
+++ Week-2/numbers.js
@@ -0,0 +1,17 @@
+// JavaScript has one number type for all numeric values.
+
+let butterFlyCandy = 0.1;
+let butterFlyCandyWithCream = 0.2;
+
+console.log("Buying both candy", 0.1 + 0.2);
+console.log("Buying both candy", 0.1 + 0.2 === 0.3);
+
+const unitPricePerGram = 0.03;
+const grams = 150;
+const total = unitPricePerGram * grams;
+console.log("twoButterFlyCandy", butterFlyCandy + butterFlyCandy);
+console.log(
+  "buyBothCandyAndReturnOne",
+  butterFlyCandy + butterFlyCandy - butterFlyCandy,
+);
+console.log(total);
--- Week-2/strings.js
+++ Week-2/strings.js
@@ -1,7 +1,20 @@
-let str1 = "Hello";
-let str2 = "World!";
+// What is a String?
+
+let str1 = "What is String?";
+let str2 = "Astring is a sequence of charaters wrapped in!";
 
 console.log(str1 + str2);
 console.log(str1 + " " + str2);
 
-const studentName = "Richard";
+// 1. String Length
+
+const studentName = "RichardNaing"; // length count space as a 1 length
+console.log(studentName.length);
+
+// 2. Accessing Characters
+
+let fileName = "20260125AAPL_Trade_FIle.log";
+console.log(fileName[0]); // 2 (first charater)
+console.log(fileName[5]); // 1 is the fith number.
+console.log(fileName.startsWith(2026));
+console.log(fileName.endsWith(File));
