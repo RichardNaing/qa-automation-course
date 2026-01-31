f5adb9bf949b1a118daa495c6730c688217d4853
Richard Naing <richardbbnaing@gmail.com>
2026-01-30 18:04:15 -0700

    Week 3 Update

--- Week-3/for-loop.js
+++ Week-3/for-loop.js
@@ -1,14 +1,19 @@
-// 1. for -loop
+// // 1. for -loop
 
-console.log("1");
-console.log("2");
-console.log("3");
-console.log("4");
-console.log("5");
-console.log("6");
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("5");
+// console.log("6");
 
-let i = 1;
-for (let i = 1; i <= 5; i++) {
-  // i= index, first count 1, last count 5
-  console.log("count:", i);
+// let i = 1;
+// for (let i = 1; i <= 5; i++) {
+//   // i= index, first count 1, last count 5
+//   console.log("count:", i);
+// }
+
+let Task1 = ["Apple", "Intel", "Tesla", "NVDA", "AMD"];
+for (let i = 0; i < Task1.length; i++) {
+  console.log("Task 1:", Task1[i]);
 }
