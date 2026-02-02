3b18ce7605351724483260febbed660f245d4cb6
Richard Naing <richardbbnaing@gmail.com>
2026-02-01 22:07:22 -0700

    Week3 Update

--- Week-3/week3-assignment2.js
+++ Week-3/week3-assignment2.js
@@ -25,7 +25,6 @@ stockListValue(["Apple", "Intel", "Tesla", "NVDA", "AMD"]);
 //____________________________________________________
 function firstAndLast(Task3) {
   if (Task3.lenght === 0) {
-    console.log("Array is empty");
     return;
   }
   console.log("First element:", Task3[0]);
@@ -41,13 +40,16 @@ firstAndLast(["Teach Stock", "RealEstate Stock", "Material Stock"]);
 // **Use a `while` loop**
 
 // 1. Takes a **string**
-const stockExchange = ["NYC", "Nasdaq", "CME"];
-let i = 0; // 2. Uses a `while` loop
-while (i < stockExchange.length) {
-  console.log("Stock Exchange Name:", stockExchange[i]);
-  i++;
+const stockExchange = "NewYorkExchange";
+function hereWeGo(stockExchange) {
+  let i = 0; // 2. Uses a `while` loop
+  while (i < stockExchange.length) {
+    console.log("Stock Exchange Name:", stockExchange[i]);
+    i++;
+  }
 }
 
+hereWeGo(stockExchange);
 // PART 3: for LOOP + CONTROL FLOW
 //____________________________________________________
 // Task 5: Stop When Found
@@ -60,11 +62,32 @@ while (i < stockExchange.length) {
 /// 1. Takes an array and a value to search for
 // 2. Loops through the array using a `for` loop
 // 3. Stops the loop when the value is found
-const person = {
-  name: "Alice",
-  age: 25,
-  isStudent: true,
-};
-for (let key in person) {
-  console.log(key + ":" + person[key]);
+const phoneModel = ["Apple", "Google", "Sony", "Sumsaung"];
+const findModel = "Sony";
+
+function findMyphone(phoneList, brand) {
+  for (let i = 0; i < phoneList.length; i++) {
+    console.log(`Find: ${phoneList[i]}`);
+    if (phoneList[i] === brand) {
+      console.log("Value found! Stopping Loop.");
+      break;
+    }
+  }
 }
+
+findMyphone(phoneModel, findModel);
+
+// PART 4: `for...of` LOOP (Value Access)
+// ### 🔂 Task 6: Print Values
+// **Use a `for...of` loop**
+// Write a function that:
+// 1. Takes an array
+// 2. Prints each value
+const students = ["MieMie", "Kyaw", "Eric", "John"];
+
+function findStudent(nameOfStudent) {
+  for (const nameOfStudent of students) {
+    console.log("Here is the Name:", nameOfStudent);
+  }
+}
+findStudent(students);
