85ccfcf78741f9e28ced48a88e76aeb45f1fb91a
Richard Naing <richardbbnaing@gmail.com>
2026-02-10 20:15:14 -0700

    Week5 Update

---
+++ Week-5/oop-practice.js
@@ -0,0 +1,10 @@
+class Car {
+    #fuelLevel = 100;
+    constructor ( brand, price) {
+        this.brand = brand;
+        this.price = price;
+    }
+    drive(reducesFuel) {
+        if (reducesFuel >= 10)
+    }
+}
